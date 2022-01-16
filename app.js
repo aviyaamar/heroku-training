edee45c501c8722d417ee3f0a333cc3399fbc040
aviyaamar <aviyaamar1@gmail.com>
2022-01-16 17:20:01 +0200

    fixed

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(5000, () => {
-    console.log('Server is up on port 5000.')
-})
+app.listen(process.env.PORT || 5000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
